0121168faa23dc781ed97acdc0c67abede6ed7fb
ardapekcan <ardapekcan@example.com>
2026-04-01 19:18:51 +0300

    fix: update vite.config.js and eslint config for Node.js globals

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
+  // Build-time environment variables
   const env = loadEnv(mode, process.cwd(), "VITE_");
 
   return {
